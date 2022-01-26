5e36fe3da2c1313de4ddecaffa8989c5c347db2a
Alistoteless <guvenaltinali@gmail.com>
2022-01-26 07:05:08 +0300

    isparkApp

---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+import ParksList from './ParksList'
+import ParksInsert from './ParksInsert'
+import ParksUpdate from './ParksUpdate'
+import Login from './Login'
+import Home from './Home'
+export { ParksList, ParksInsert, ParksUpdate, Login, Home }
